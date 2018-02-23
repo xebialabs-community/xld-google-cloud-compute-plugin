90bacefedfb860cd2d07449420147da0246943c1
Benoit Moussaud <bmoussaud@xebialabs.com>
2018-02-23 17:32:06 +0100

    TODO: template

--- src/main/java/org/jclouds/googlecomputeengine/domain/JCloudGoogleCompute.java
+++ src/main/java/org/jclouds/googlecomputeengine/domain/JCloudGoogleCompute.java
@@ -1,10 +1,10 @@
 /**
  * Copyright 2018 XEBIALABS
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package org.jclouds.googlecomputeengine.domain;
@@ -71,7 +71,7 @@ public class JCloudGoogleCompute {
         return o.selfLink().toString();
     }
 
-    public String createInstance(String instanceName, String imageName, String imageProject, String machine, String zone, String externalAddress, Map<String,String> metadata) {
+    public String createInstance(String instanceName, String imageName, String imageProject, String machine, String zone, String externalAddress, Map<String, String> metadata) {
         URI machineTypeURL = googleApi.machineTypesInZone(zone).get(machine).selfLink();
         System.out.println("machineTypeURL = " + machineTypeURL);
 
@@ -105,6 +105,12 @@ public class JCloudGoogleCompute {
 
         Operation o = getInstanceApi(zone).create(newInstance);
         return o.selfLink().toString();
+    }
+
+    public String createInstanceGroup(String baseInstanceName, String templateName, String zone, int size) {
+
+        //TODO
+        return null;
     }
 
     private Instance.NetworkInterface.AccessConfig getDefaultAccessConfig() {
